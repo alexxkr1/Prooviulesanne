aa01c204e22dba59de7d0c5d28e840eb02c85903
Alexxkr1 <Alexandroskrenstrauch@gmail.com>
2022-10-02 11:21:04 +0300

    Created Controller and View for making Events

---
+++ Prooviulesanne/Controllers/EventsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proov.Data;
+using Prooviulesanne.Models.Domain;
+
+namespace Prooviulesanne.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EventsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateEvent()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateEvent([Bind("Id,ÜrituseNimi,ToimumisAeg,ToimumiseKoht,LisaInfo")] Event events)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(events);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(events);
+        }
+    }
+}
