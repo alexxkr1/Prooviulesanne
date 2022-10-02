73da8bbf96f31e96fa40d963bd97bb0450c9b873
Alexxkr1 <Alexandroskrenstrauch@gmail.com>
2022-10-02 19:28:11 +0300

    Revert "Revert "Created Views and Controller for Citizens""
    This reverts commit 3a9a6dba3d0f528ac0b16e624d662964550ea906.

---
+++ Prooviulesanne/Controllers/CitizensController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Proov.Data;
+using Prooviulesanne.Models.Domain;
+
+namespace Prooviulesanne.Controllers
+{
+    public class CitizensController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CitizensController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Index([Bind(Prefix = "id")] int EventId)
+        {
+            var model = _context.Event
+                .Include(e => e.Citizens)
+                .FirstOrDefault(r => r.Id == EventId);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            return View(model);
+        }
+
+
+
+        [HttpGet]
+        public ActionResult CreateCitizen(int EventId)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateCitizen(int EventId, Citizen participant)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Employee.Add(participant);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Citizen", new { id = participant.EventId });
+                //return RedirectToAction("Index", "Home");
+            }
+            return View(participant);
+        }
+
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var osaleja = await _context.Employee
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (osaleja == null)
+            {
+                return NotFound();
+            }
+
+            return View(osaleja);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var current = _context.Employee.Find(id);
+            var participant = await _context.Employee.FindAsync(id);
+            _context.Employee.Remove(participant);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Details", "Home", new { id = current.EventId });
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var model = _context.Employee.Find(id);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, Citizen participant)
+        {
+            if (id != participant.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                var current = _context.Employee.Find(id);
+                current.FirstName = participant.FirstName;
+                current.LastName = participant.LastName;
+                current.IdentificationNumber = participant.IdentificationNumber;
+                current.PaymentType = participant.PaymentType;
+                current.Details = participant.Details;
+                current.EventId = participant.EventId;
+                _context.SaveChanges();
+                return RedirectToAction("Details", "Home", new { id = current.EventId });
+            }
+            return View(participant);
+        }
+    }
+}
